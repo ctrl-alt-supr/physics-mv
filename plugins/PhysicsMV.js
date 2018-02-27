fd59067197bfe51683ce7cb8e9201933f8e1aa98
ctrl-alt-supr <appparaalex@gmail.com>
2018-02-27 07:55:40 +0100

    Added syntax highlighting files to the proyect

---
+++ plugins/PhysicsMV.js
@@ -0,0 +1,13 @@
+//=============================================================================
+// PhysicsMV.js
+//=============================================================================
+/*:
+ * @plugindesc 2D Physics integration for RPGMaker MV.
+ * @author Alex
+ *
+ * @help Sin comandos de plugin.
+ */
+(function() {
+
+
+})();
